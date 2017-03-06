0bf6779162b4833cdfe0f8e219abcd147bd0f011
ARNOLAPTOP\Arnaud <arnaud.petitcol@gmail.com>
2017-03-06 16:13:59 +0100

    Variable name correction

--- ODM_alpha/app_sv_bdd.cpp
+++ ODM_alpha/app_sv_bdd.cpp
@@ -18,8 +18,8 @@ app_sv_bdd::app_sv_bdd()
 
 void app_sv_bdd::start(){
     QThread::sleep(1);
-    if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Start of app_sv_bdd on:";
-    if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << Q_FUNC_INFO << QThread::currentThreadId();
+    if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Start of app_sv_bdd on:";
+    if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << Q_FUNC_INFO << QThread::currentThreadId();
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName(DB_HOSTNAME);
     db.setUserName(DB_USERNAME);
@@ -27,16 +27,16 @@ void app_sv_bdd::start(){
     db.setDatabaseName(DB_NAME);
     if(db.open())
     {
-        if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Connected to:";
-        if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << db.hostName();
+        if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Connected to:";
+        if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << db.hostName();
         is_running = true;
         if(this->init())
             this->loop();
     }
     else
     {
-        if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Connecion failed";
-        if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << db.lastError().text();
+        if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Connecion failed";
+        if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << db.lastError().text();
     }
     db.close();
 }
@@ -47,19 +47,19 @@ bool app_sv_bdd::init(){
     if(query.exec("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'odm_db' AND TABLE_NAME = 'main';"))
     {
         if(query.next()){
-            if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "The DB table 'main' exists !";
+            if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "The DB table 'main' exists !";
             return_b = true;
         }
         else {
-            if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Trying to create table 'main'...";
-            if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "CREATE TABLE main ("+TABLE_ID+" int, "+TABLE_LONG+" double, "+TABLE_LAT+" double, "+TABLE_ALT+" double, position"+TABLE_DTE+" datetime);";
+            if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Trying to create table 'main'...";
+            if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "CREATE TABLE main ("+TABLE_ID+" int, "+TABLE_LONG+" double, "+TABLE_LAT+" double, "+TABLE_ALT+" double, position"+TABLE_DTE+" datetime);";
             if (query.exec("CREATE TABLE main ("+TABLE_ID+" int, "+TABLE_LONG+" double, "+TABLE_LAT+" double, "+TABLE_ALT+" double, position"+TABLE_DTE+" datetime);"))
             {
-                if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "created";
+                if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "created";
                 return_b = true;
             }
             else
-                if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Error while creating the table.";
+                if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Error while creating the table.";
         }
     }
     return return_b;
@@ -77,14 +77,14 @@ void app_sv_bdd::loop(){
                 altitude = state[i].take(TABLE_ALT).toString();
                 date = state[i].take(TABLE_DTE).toString();
 
-                if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "INSERT INTO `main`(`"+TABLE_ID+"`, `"+TABLE_LONG+"`, `"+TABLE_LAT+"`, `"+TABLE_ALT+"`, `positiondate`) VALUES ("+id_drone+","+longitude+","+latitude+","+altitude+",\""+date+"\")";
+                if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "INSERT INTO `main`(`"+TABLE_ID+"`, `"+TABLE_LONG+"`, `"+TABLE_LAT+"`, `"+TABLE_ALT+"`, `positiondate`) VALUES ("+id_drone+","+longitude+","+latitude+","+altitude+",\""+date+"\")";
                 if(query.exec("INSERT INTO `main`(`"+TABLE_ID+"`, `"+TABLE_LONG+"`, `"+TABLE_LAT+"`, `"+TABLE_ALT+"`, `positiondate`) VALUES ("+id_drone+","+longitude+","+latitude+","+altitude+",\""+date+"\")"))
                 {
-                    if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Insert success";
+                    if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Insert success";
                 }
                 else{
-                    if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << "Insert Failed !";
-                    if (DEBUG_ENABLE) qDebug() << MARQUEUR_DEBUG << query.lastError().text();
+                    if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << "Insert Failed !";
+                    if (DEBUG_ENABLE) qDebug() << MARKER_DEBUG << query.lastError().text();
                 }
             }
         }
